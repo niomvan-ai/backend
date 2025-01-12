365e393f02b876099e6dc1f0c2dcaf84026a18b2
Nimit Jalan <nimitjalan2013@gmail.com>
2025-01-12 12:03:01 +0400

    Initial Commit

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from .models import User
 from rest_framework.test import APIClient
 from rest_framework import status
 
