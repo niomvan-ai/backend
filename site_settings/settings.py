751af06978980dbc8c55a45c989df7c75ba105d3
Nimit Jalan <nimitjalan2013@gmail.com>
2025-01-18 08:06:04 +0400

    fix: update ALLOWED_HOSTS to use the production URL for deployment

--- site_settings/settings.py
+++ site_settings/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = "django-insecure-nma=xi6x2p-crjg^ifqqkapyu1qjd0l=+wn)-rijk_o%$!k3w_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["niomvanai.pythonanywhere.com"]
+ALLOWED_HOSTS = ["https://web-production-724a2.up.railway.app"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
